873cef2bb8f852487c6f1c5ac3b9f815cea563ca
Guilherme (Gillie) <miscildes@gmail.com>
2021-08-26 14:54:00 -0300

    Task 8 - criação de componentes e implementação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContateNosComponent } from './contate-nos/contate-nos.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "inicio", pathMatch:"full"},
+  {path: "entrar", component: EntrarComponent},
+  {path: "cadastrar", component: CadastrarComponent},
+  {path: "inicio", component: InicioComponent},
+  {path: "contate-nos", component: ContateNosComponent},
+  {path: "sobre-nos", component: SobreNosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContateNosComponent } from './contate-nos/contate-nos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    SobreNosComponent,
+    ContateNosComponent,
+    InicioComponent,
+    CadastrarComponent,
+    EntrarComponent,
   ],
   imports: [
     BrowserModule,
